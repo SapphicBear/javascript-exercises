d0805c01ddf068e0a6f05ca43b5556575197a4d6
SapphicBear <235136735+SapphicBear@users.noreply.github.com>
2025-11-03 19:20:40 +0100

    Finish 07_reverseString lesson!
    Edit reverseString.js and reverseString.spec.js and finish the lesson!

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split('');
+    reversed = array.reverse();
+    return reversed.join("");
 };
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
