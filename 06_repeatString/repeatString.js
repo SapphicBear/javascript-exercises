b181e9d0c37097bff7cfd660b3d80a6f7df111d7
SapphicBear <235136735+SapphicBear@users.noreply.github.com>
2025-11-03 19:04:33 +0100

    Finish 06-repeatString assignment
    Edit repeatString.js and .spec.js to finish the assignment.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const stringArray = [];
+    for (let i = 0; i < num; i++) {
+        stringArray.push(string);
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    return stringArray.join("");
 };
+const string1 = "goodbye";
+
+repeatString(string1, -1);
 
 // Do not edit below this line
 module.exports = repeatString;
