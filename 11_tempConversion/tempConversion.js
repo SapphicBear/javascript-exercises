746adef677107c0f946ee0578fd4f148e4652438
SapphicBear <235136735+SapphicBear@users.noreply.github.com>
2025-11-04 16:23:47 +0100

    complete exercise 11 tempConversion
    Edit and finish the lesson 11_tempConversion for the javascript
    exercises!

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    const fToC = (temp - 32) * 0.555555555;
+    return Math.round(fToC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const cToF = (temp * 1.8) + 32;
+  return Math.round(cToF * 10) / 10;
 };
 
 // Do not edit below this line
