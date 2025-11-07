5eddccd3f5f285a937e27bc851977856b6a6fbb4
SapphicBear <235136735+SapphicBear@users.noreply.github.com>
2025-11-07 17:05:31 +0100

    Complete exercise 13
    Edit and complete the exercise for the lesson

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+function removePunc (string) {
+        let punctuation = /[\.,?! ]/g;
+        let newText = string.replace(punctuation, "");
+        return newText;
+    }
+const palindromes = function (string) {
+    const newText = removePunc(string);
+    const reversed = newText
+                    .split("")
+                    .reverse()
+                    .join("");
+    if (reversed.toLowerCase() === newText.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
